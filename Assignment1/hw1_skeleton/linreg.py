2cd348f3842ef73bcee349c7991720a5d3c9ab54
Nguyễn Thái Bảo <104883071+bao4vui@users.noreply.github.com>
2023-11-28 00:14:50 +0700

    Update linreg.py

--- Assignment1/hw1_skeleton/linreg.py
+++ Assignment1/hw1_skeleton/linreg.py
@@ -1,14 +1,7 @@
-'''
-    TEMPLATE FOR MACHINE LEARNING HOMEWORK
-    AUTHOR Eric Eaton, Vishnu Purushothaman Sreenivasan
-'''
 import numpy as np
-from numpy import linalg as LA
 import matplotlib.pyplot as plt
 
-
 class LinearRegression:
-
     def __init__(self, init_theta=None, alpha=0.01, n_iter=100):
         '''
         Constructor
@@ -17,7 +10,6 @@ class LinearRegression:
         self.n_iter = n_iter
         self.theta = init_theta
         self.JHist = None
-    
 
     def gradientDescent(self, X, y, theta):
         '''
@@ -29,29 +21,29 @@ class LinearRegression:
         Returns:
             the final theta found by gradient descent
         '''
-        n,d = X.shape
+        n, d = X.shape
         self.JHist = []
-        for i in xrange(self.n_iter):
-            self.JHist.append( (self.computeCost(X, y, theta), theta) )
-            print "Iteration: ", i+1, " Cost: ", self.JHist[i][0], " Theta: ", theta
-            # TODO:  add update equation here
-
+        for i in range(self.n_iter):
+            self.JHist.append((self.computeCost(X, y, theta), theta))
+            print("Iteration:", i + 1, "Cost:", self.JHist[i][0], "Theta:", theta)
+            # Update equation for gradient descent
+            theta = theta - (self.alpha / n) * X.T.dot(X.dot(theta) - y)
         return theta
-    
 
     def computeCost(self, X, y, theta):
         '''
         Computes the objective function
         Arguments:
-          X is a n-by-d numpy matrix
-          y is an n-dimensional numpy vector
-          theta is a d-dimensional numpy vector
+            X is a n-by-d numpy matrix
+            y is an n-dimensional numpy vector
+            theta is a d-dimensional numpy vector
         Returns:
-          a scalar value of the cost  
-              ** make certain you don't return a matrix with just one value! **
+            a scalar value of the cost
+            ** make certain you don't return a matrix with just one value! **
         '''
-        # TODO: add objective (cost) equation here
-    
+        n = len(y)
+        cost = (1 / (2 * n)) * np.sum(np.square(X.dot(theta) - y))
+        return cost
 
     def fit(self, X, y):
         '''
@@ -61,11 +53,10 @@ class LinearRegression:
             y is an n-dimensional numpy vector
         '''
         n = len(y)
-        n,d = X.shape
-        if self.theta==None:
-            self.theta = np.matrix(np.zeros((d,1)))
-        self.theta = self.gradientDescent(X,y,self.theta)    
-
+        _, d = X.shape
+        if self.theta is None:
+            self.theta = np.zeros((d, 1))
+        self.theta = self.gradientDescent(X, y, self.theta)
 
     def predict(self, X):
         '''
@@ -75,4 +66,4 @@ class LinearRegression:
         Returns:
             an n-dimensional numpy vector of the predictions
         '''
-        # TODO:  add prediction function here
+        return X.dot(self.theta)
